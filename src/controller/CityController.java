764585474ad8734c9d527f2859bce7cfdbcde981
MikiKru <michal_kruczkowski@o2.pl>
2019-02-22 11:45:22 +0100

    DB City

---
+++ src/controller/CityController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CityController {
+    Map<String, Integer> ids = new HashMap<>();
+
+
+
+
+}
